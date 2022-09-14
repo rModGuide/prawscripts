4e9c6e31772985e774733170354ba222095f58f1
Ka Hung Lee <lee.kahung414@gmail.com>
2022-09-14 02:28:24 -0700

    Updating README and setup.py for version 0.0.9

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.8'
+VERSION = '0.0.9'
 DESCRIPTION = 'Wrapper for PRAW scripts built over the years by the community and mods'
 
 # Setting up
